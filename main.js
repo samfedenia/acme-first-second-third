e8d6e069030c960dad10c45c713b3fe843f555cf
Sam Fedenia <sam@fedenia.xyz>
2021-02-03 10:46:30 -0600

    changed moveIt function to use closure

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// data
 const slots = ["first", "second", "third"];
 
 const users = [
@@ -24,75 +25,79 @@ const users = [
   },
 ];
 
+// callback function to move li divs from one ul to target ul
 function moveIt(targetId) {
-  const parent = this.parentElement;
-  const selector = targetId + " > ul";
-  const targetDiv = document.querySelector(selector);
+  return function () {
+    // this context is button div that was clicked due to closure
+    const parent = this.parentElement;
+    const selector = targetId + " > ul";
+    const targetDiv = document.querySelector(selector);
 
-  if (parent.querySelectorAll("li.selected").length > 0) {
-    selectedNames = [...parent.querySelectorAll("li.selected")];
-    selectedNames.forEach((name) => {
-      name.parentElement.removeChild(name);
-      targetDiv.appendChild(name);
-    });
+    if (parent.querySelectorAll("li.selected").length > 0) {
+      selectedNames = [...parent.querySelectorAll("li.selected")];
+      selectedNames.forEach((name) => {
+        name.parentElement.removeChild(name);
+        targetDiv.appendChild(name);
+      });
+    }
+  };
+}
+
+// callback function for selecting/deselecting li divs
+function selectName(elem) {
+  elem = elem.target;
+  elemClasses = elem.classList;
+
+  if ([...elemClasses].includes("selected")) {
+    elemClasses.remove("selected");
+  } else {
+    elemClasses.add("selected");
   }
 }
 
+// loop over each slot in slots array and create buttons and divs
 for (let i = 0, len = slots.length; i < len; i++) {
+  // set up elements and properties
   btnLeft = document.createElement("button");
   btnRight = document.createElement("button");
   label = document.createElement("h2");
+  ul = document.createElement("ul");
   btnRight.innerText = ">";
   btnLeft.innerText = "<";
+  label.innerText = slots[i];
 
-  switch (slots[i]) {
-    case "first":
-      btnRight.classList.add("selected");
-      btnRight.addEventListener("click", function () {
-        moveIt.call(this, "#second");
-      });
-      break;
-    case "second":
-      btnRight.classList.add("selected");
-      btnRight.addEventListener("click", function () {
-        moveIt.call(this, "#third");
-      });
-      btnLeft.classList.add("selected");
-      btnLeft.addEventListener("click", function () {
-        moveIt.call(this, "#first");
-      });
-      break;
-    case "third":
-      btnLeft.classList.add("selected");
-      btnLeft.addEventListener("click", function () {
-        moveIt.call(this, "#second");
-      });
-      break;
-  }
-
-  ul = document.createElement("ul");
+  // select corresponding column div and append elements
   div = document.querySelector("#" + slots[i]);
   div.appendChild(btnRight);
   div.appendChild(btnLeft);
-  label.innerText = slots[i];
   div.appendChild(label);
   div.appendChild(ul);
 
+  // set button properties/event listeners in each column
+  switch (slots[i]) {
+    case "first":
+      btnRight.classList.add("selected");
+      btnRight.addEventListener("click", moveIt("#second"));
+      break;
+    case "second":
+      btnRight.classList.add("selected");
+      btnRight.addEventListener("click", moveIt("#third"));
+      btnLeft.classList.add("selected");
+      btnLeft.addEventListener("click", moveIt("#first"));
+      break;
+    case "third":
+      btnLeft.classList.add("selected");
+      btnLeft.addEventListener("click", moveIt("#second"));
+      break;
+  }
+
+  // loop over each user in users to create li divs
   for (user of users) {
     if (div.id === user.slot) {
       li = document.createElement("li");
-      li.addEventListener("click", (elem) => {
-        elem = elem.target;
-        elemClasses = elem.classList;
-
-        if ([...elemClasses].includes("selected")) {
-          elemClasses.remove("selected");
-        } else {
-          elemClasses.add("selected");
-        }
-      });
-
+      li.addEventListener("click", selectName);
       li.innerText = user.name;
+
       if (user.selected) {
         li.classList.add("selected");
       }
